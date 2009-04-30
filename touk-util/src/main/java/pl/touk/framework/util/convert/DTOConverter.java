e5f99efc27f5fb10b46f5094151f04a28f7e20ad
Jakub Nabrdalik <jnb@touk.pl>
2009-04-30 12:38:53 +0000

    DTOConverter changed to DTODozerConverter (part2, complete)
    git-svn-id: http://top.touk.pl/svn/toukframework/trunk@72951 a7b80be8-48eb-4681-8a4f-b7484ab9f0e7

---
+++ touk-util/src/main/java/pl/touk/framework/util/convert/DTOConverter.java
@@ -0,0 +1,45 @@
+package pl.touk.framework.util.convert;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * 
+ * @author  Ula Trzaskowska
+ */
+public interface DTOConverter {
+    
+    /**
+     * Converts source domain object into desired destination DTO class instance.
+     *
+     * @param sourceObject - domain class to convert
+     * @param destination class - desired class name
+     *
+     * @return instance of destinationClass, mapped with source object properties.
+     */
+
+    public <FROM, TO> TO convert(FROM sourceObject, java.lang.Class<TO> destinationClass);
+
+    /**
+     * Converts domain objects collection into a collection of desired DTO objects.
+     *
+     * @param sourceObjects - domain class collection
+     * @param destinationClass - desired class name, instances of which will be in the returning collection
+     *
+     * @return a collection of DTO objects
+     */
+
+    public <FROM extends Collection, TO> List<TO> convert(FROM sourceObjects, java.lang.Class<TO> destinationClass);
+
+    /**
+     * Maps source object into destination object, without creating new instance
+     *
+     * @param sourceObject
+     * @param destinationObject
+     *
+     * @return destinationObject, mapped with source objects properties.
+     */
+
+    public <FROM, TO> TO convert(FROM sourceObject, TO destinationObject);
+
+}
