e17f1c4111992f92a4d39947ed1434762f8017db
Mateusz Lipczyński <mlp@touk.pl>
2009-07-30 09:24:14 +0000

    EWKA-315 configuration moved to TouK framework.
    git-svn-id: http://top.touk.pl/svn/toukframework/trunk@74411 a7b80be8-48eb-4681-8a4f-b7484ab9f0e7

---
+++ touk-util/src/sql/configuration.sql
@@ -0,0 +1,6 @@
+    create table configuration (
+        key varchar2(100 char) not null,
+        value varchar2(255 char),
+        primary key (key)
+    );
+                                                
